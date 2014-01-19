dc8cd21612e20936fed2554ca75e9aeba4bcf71a
asegorov <ase8487@gmail.com>
2014-01-19 14:42:32 +0400

    TESTBD-0001 Django App

---
+++ mysite/testbd/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Transaction(models.Model):
+    class Type(object):
+        DEPOSIT = 1
+        WITHDRAW = 2
+
+        choices = (
+	        (DEPOSIT, u'out'),
+            (WITHDRAW, u'in'),
+        )
+
+    date = models.DateTimeField(auto_now_add=True)
+    type = models.SmallIntegerField(choices=Type.choices)
+
+    class Meta(object):
+        get_latest_by = 'date'
+        ordering = ['-date']
+
+
+class Purchase(Transaction):
+    original_price = models.DecimalField(max_digits=12, decimal_places=2, null=True)
+
+    def __unicode__(self):
+	    return '%s %s %s' % (self.date, self.type, self.original_price)
